a15584f2fe5fff658b0137ce1870fe51a436b876
Matt Jones <mattjones@yieldbot.com>
2015-06-03 10:57:32 -0400

    deploy

--- lib/sensu-plugins-graphite.rb
+++ lib/sensu-plugins-graphite.rb
@@ -1,14 +1,1 @@
 require 'sensu-plugins-graphite/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsGraphite
-  class << self
-  end
-
-  class << self
-  end
-end
--- lib/sensu-plugins-graphite/version.rb
+++ lib/sensu-plugins-graphite/version.rb
@@ -1,28 +1,9 @@
-require 'json'
-
-# encoding: utf-8
 module SensuPluginsGraphite
-  # This defines the version of the gem
   module Version
     MAJOR = 0
     MINOR = 0
     PATCH = 1
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
-
-    NAME   = 'sensu-plugins-graphite'
-    BANNER = "#{NAME} v%s"
-
-    module_function
-
-    def version
-      format(BANNER, VER_STRING)
-    end
-
-    def json_version
-      {
-        'version' => VER_STRING
-      }.to_json
-    end
   end
 end
